0f1186607b38149a3466b6c9a365976cfc2d56fd
justinmurphycalgary <justinmurphyis@gmail.com>
2022-02-21 17:38:43 -0700

    client refactor

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: '127.0.0.1',
+    port: '50541',
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log('Message recieved form snakeserver :>> ', data);
+  })
+
+  return conn;
+};
+
+module.exports = connect;
--- play.js
+++ play.js
@@ -1,20 +1,5 @@
 const net = require("net");
+const connect = require('./client');
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '127.0.0.1',
-    port: '50541',
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-
-  conn.on('data', (data) => {
-    console.log('Message recieved form snakeserver :>> ', data);
-  })
-
-  return conn;
-};
 console.log("Connecting ...");
 connect();
